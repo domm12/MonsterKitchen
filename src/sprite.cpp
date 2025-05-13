a04cd85b7b442d2bfe3ce333bf4f2fe640ecf18a
dom <dommod01@gmail.com>
2025-05-13 10:30:17 +0300

    basic controller implementation (move, act)

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -12,32 +12,87 @@ void Sprite::move(int dx, int dy){
     pos_y += dy;
 }
 
-void use(){}
+void Sprite::action(){}
 
-Controller(){}
+Controller():facing(UP){
+    source = Rectangle(
+        (float)(facing * BLOCK_SIZE),
+        0,
+        (float)BLOCK_SIZE,
+        (float)BLOCK_SIZE
+    );
+}
+
+void Controller::draw(){
+    DrawTextureRec(texture, source, {pos_x, pos_y}, WHITE);
+}
+
 void Controller::move(){
+    bool ch_face = false;
     if((IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) && pos_x < MAX_MAP_WIDTH - 1){
         Sprite* new_space = map[pos_y][pos_x + 1];
-        if(new_space && new_space.walkable){
+        ch_face = (facing != RIGHT);
+        facing = RIGHT;
+        if(new_space && new_space->walkable){
             move(1, 0);
         }
     }
     if((IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) && pos_x > 0){
         Sprite* new_space = map[pos_y][pos_x - 1];
-        if(new_space && new_space.walkable){
-            move(-1, 0);
+        ch_face = (facing != LEFT);
+        facing = LEFT;
+        if(new_space && new_space->walkable){
+            move(-1, 0); 
         }
     }
     if((IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) && pos_y > 0){
         Sprite* new_space = map[pos_y - 1][pos_x];
-        if(new_space && new_space.walkable){
+        ch_face = (facing != UP);
+        facing = UP;
+        if(new_space && new_space.->alkable){
             move(0, -1);
         }
     }
     if((IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) && pos_y < MAX_MAP_HEIGHT - 1){
         Sprite* new_space = map[pos_y + 1][pos_x];
-        if(new_space && new_space.walkable){
+        ch_face = (facing != DOWN);
+        facing = DOWN;
+        if(new_space && new_space->walkable){
             move(0, 1);
+        }
+    }
+    if(ch_face){
+        source = Rectangle(
+            (float)(facing * BLOCK_SIZE),
+            0,
+            (float)BLOCK_SIZE,
+            (float)BLOCK_SIZE
+        );
+    }
+}
+
+void Controller::act(){
+    if(IsKeyPressed(KEY_E)){
+        new_pos_x = pos_x;
+        new_pos_y = pos_y;
+        switch(facing){
+            case UP:
+                new_pos_y -= 1;
+                break;
+            case LEFT:
+                new_pos_x -= 1;
+                break;
+            case RIGHT:
+                new_pos_x += 1;
+                break;
+            case DOWN:
+                new_pos_y += 1;
+                break;
+        }
+        if(new_pos_x >= MAX_MAP_WIDTH || new_pos_x < 0 || new_pos_y >= MAX_MAP_HEIGHT || new_pos_y < 0) return;
+        Sprite* new_space = map[new_pos_y][new_pos_x];
+        if(new_space){
+            new_space->action();
         }
     }
 }
--- src/sprite.h
+++ src/sprite.h
@@ -4,6 +4,13 @@
 #define MAX_MAP_WIDTH 30
 #define MAX_MAP_HEIGHT 20
 
+enum Directions{
+    UP,
+    LEFT,
+    RIGHT,
+    DOWN
+}
+
 class Sprite;
 
 Sprite* map[MAX_MAP_HEIGHT][MAX_MAP_WIDTH];
@@ -19,11 +26,15 @@ class Sprite{
     Sprite(Texture2D t, int x, int y, bool w, int l);
     void draw();
     void move(int dx, int dy);
-    virtual void use();
-}
+    virtual void action();
+};
 
 class Controller : public Sprite{
+    Directions facing;
+    Rectangle source;
     public:
     Controller();
+    void draw();
     void move();
-}
+    void act();
+};
