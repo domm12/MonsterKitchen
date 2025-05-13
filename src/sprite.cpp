4d35d53f092810b9bbd220c27ffee40db5627eb3
dom <dommod01@gmail.com>
2025-05-13 09:18:46 +0300

    Start of Controller

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,8 +1,43 @@
 #include "raylib.h"
 #include "sprite.h"
 
-Sprite::Sprite(Texture2D t, int x, int y, bool w):texture(t), pos_x(x), pos_y(y), walkable(w){}
+Sprite::Sprite(Texture2D t, int x, int y, bool w):texture(t), pos_x(x), pos_y(y), walkable(w), layer(l){}
 
-Sprite::draw(){
+void Sprite::draw(){
     DrawTexture(texture, pos_x, pos_y, WHITE);
+}
+
+void Sprite::move(int dx, int dy){
+    pos_x += dx;
+    pos_y += dy;
+}
+
+void use(){}
+
+Controller(){}
+void Controller::move(){
+    if((IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) && pos_x < MAX_MAP_WIDTH - 1){
+        Sprite* new_space = map[pos_y][pos_x + 1];
+        if(new_space && new_space.walkable){
+            move(1, 0);
+        }
+    }
+    if((IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) && pos_x > 0){
+        Sprite* new_space = map[pos_y][pos_x - 1];
+        if(new_space && new_space.walkable){
+            move(-1, 0);
+        }
+    }
+    if((IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) && pos_y > 0){
+        Sprite* new_space = map[pos_y - 1][pos_x];
+        if(new_space && new_space.walkable){
+            move(0, -1);
+        }
+    }
+    if((IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) && pos_y < MAX_MAP_HEIGHT - 1){
+        Sprite* new_space = map[pos_y + 1][pos_x];
+        if(new_space && new_space.walkable){
+            move(0, 1);
+        }
+    }
 }
--- src/sprite.h
+++ src/sprite.h
@@ -1,6 +1,12 @@
 #include "raylib.h"
 
 #define BLOCK_SIZE 64
+#define MAX_MAP_WIDTH 30
+#define MAX_MAP_HEIGHT 20
+
+class Sprite;
+
+Sprite* map[MAX_MAP_HEIGHT][MAX_MAP_WIDTH];
 
 class Sprite{
     protected:
@@ -8,8 +14,16 @@ class Sprite{
     int pos_x;
     int pos_y;
     bool walkable;
+    int layer;
     public:
-    Sprite(Texture2D t, int x, int y, bool w);
-    draw();
+    Sprite(Texture2D t, int x, int y, bool w, int l);
+    void draw();
+    void move(int dx, int dy);
+    virtual void use();
 }
 
+class Controller : public Sprite{
+    public:
+    Controller();
+    void move();
+}
